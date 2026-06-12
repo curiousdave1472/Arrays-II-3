dac2383d982f50b3b81ed4bd3ab568a8ac951f3d
curiousdave1472 <davidshanlay@gmail.com>
2026-06-12 03:30:33 +0000

    Shortest word

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-// Write your code below
+// Find the shortest element from array.
+
+var words = ["Bee", "Chicken", "Bird"]
+
+var shortestWord = [];
+
+function findShortestElement(words,short){
+
+    for (var i=0; i < words.length; i = i +1) {
+
+        if (words[i].length <= short) {
+
+            shortestWord.push(words[i]);
+        }
+    }
+    return shortestWord
+
+}
+var output = findShortestElement(words, 3)
+
+console.log(output);
+
